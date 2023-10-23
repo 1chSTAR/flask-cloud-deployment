3393a7abee1b3e97a90e437d3b705764463f8fa9
Christine Srour <christine.srour.1@gmail.com>
2023-10-23 17:20:04 +0300

    test corrections

--- tests/tests.py
+++ tests/tests.py
@@ -9,10 +9,25 @@ class FlaskAppTests(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-    def test_home_page(self):
+    def test_home_page_get(self):
+        """Test the home page with a GET request."""
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hello', response.data)  # Check if "Hello" is in the response
+        # Since we're not sure about the specific content in a GET request, 
+        # we just check for a successful response.
+        # If there's a consistent string on every GET request, you can assert for its presence.
+
+    def test_home_page_post(self):
+        """Test the home page with a POST request and a name."""
+        response = self.app.post('/', data={'name': 'aws'})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello, aws!', response.data)
+
+    def test_home_page_post_without_name(self):
+        """Test the home page with a POST request without a name."""
+        response = self.app.post('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(b'Hello,', response.data)  # Ensure no greeting when no name is provided
 
 if __name__ == '__main__':
     unittest.main()
